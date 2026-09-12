2318d995a4714a66ca824f162369119e26e081b6
Karollo6767 <karol.stanek12@gmail.com>
2026-09-12 19:42:45 +0200

    Add recognising commands (not working for now) and recognising if user typed in white space and some "continue" for program stability

---
+++ Project AirFryerRecipieApp.md
@@ -0,0 +1,11 @@
+# Project: AirFryerRecipieApp:
+## What's working:
+- [x] Adding recipies
+- [x] Displaying error communicate if user has accidentally 
+      pressed enter and wrote nothing
+
+## What's not working:
+- [ ] Recognising command to show all and 
+      formatted recipies in table
+- [ ] Escaping the program and showing the number
+      of added recipies
--- tester.py
+++ tester.py
@@ -1,9 +1,10 @@
 prompt = "\n-----------------------------------------------------"
 prompt += "\nEnter the recipie in this order:\n"
 prompt += "  'KEY: recipie_name' : 'VALUE: recipie_content'\n"
-prompt += "If you want to exit the program type 'end' or 'exit'."
+prompt += "If you want to exit the program type ['end()'] or ['exit()']."
 prompt += "\n-----------------------------------------------------"
-prompt += "\n>>>"
+prompt += "\n[~! REMEMBER ABOUT CORRECT RECIPIE FORMAT !~]"
+prompt += "\n\n>>>"
 
 recipies = {}
 formatted_recipies = []
@@ -11,14 +12,23 @@ formatted_recipies = []
 recipie_name = recipies.keys()
 recipie_content = recipies.values()
 
-def add_recipie(name, content):
-    recipies[name] = content
+recipies_count = 0
 
+app_is_running = True
+is_command = False
+
+def add_recipie(name, content):
+    global recipies_count
+    recipies[name] = content
+    recipies_count += 1
+
+# NOT WORKING 
 def show_all_formatted_recipies():
     """Display nice-formatted recipies"""
     for name, recipie in recipies.items():
-        name.append()
-        recipie.append()
+        formatted_recipies.append(name)
+        formatted_recipies.append(recipie)
+        print(formatted_recipies)
 
     for index in range(len(formatted_recipies)):
         if index % 2 == 0:
@@ -27,24 +37,47 @@ def show_all_formatted_recipies():
         elif index % 2 == 1:
             print(f"{recipie_content}")
 
-app_is_running = True
+def format_error_comunicate():
+    syntax_comunicate = "\n!!! Wrong formatted recipie.\n    Read the prompt above !!!"
+    print(syntax_comunicate)
 
+# Main app loop
 while app_is_running:
     user_input = input(prompt)
 
     user_input = user_input.lower()
-    user_input = str(user_input)
+    user_input = str(user_input) 
 
-    if user_input == 'showAll':
-        show_all_formatted_recipies()
+    # Ignore white spaces
+    if user_input == '':
+        is_command = False
+        print('\n!!! Nothing to add !!!')
+        continue
 
-    if (user_input == 'end') or (user_input == 'exit'):
-        print('>> Escaped program <<')
+    # Check if user_input DOESN'T HAVE ":" but it IS A COMMAND
+    if ":" not in user_input and is_command == True:
+        continue
+
+    # Check if user_input DOESN'T HAVE ":" but it IS > NOT < A COMMAND
+    if ":" not in user_input and is_command == False:
+        format_error_comunicate()
+        continue
+
+    # Recognise the recipies adding
+    else:
+        if (user_input != 'end()') or (user_input != 'exit()'):
+            recipie_name, recipipe_content = user_input.split(':', maxsplit=1)
+
+            add_recipie(recipie_name, recipipe_content)
+
+            print(f'\n> Saved <\n{recipie_name.capitalize()}:\n  {recipipe_content}')
+
+    if (user_input == 'end()') or (user_input == 'exit()'):
+        is_command = True
+        print('\n---------------------\n>> Escaped program <<\n---------------------')
         app_is_running = False
+        print(f"\n@| You've added {recipies_count} recipie/s |@")
 
-    elif (user_input != 'end') or (user_input != 'exit'):
-        recipie_name, recipipe_content = user_input.split(':', maxsplit=1)
-
-        add_recipie(recipie_name, recipipe_content)
-
-        print(f'\n> Saved <\n{recipie_name.capitalize()}:\n  {recipipe_content}')
+    elif user_input == 'showAll()':
+        is_command = True
+        show_all_formatted_recipies()
